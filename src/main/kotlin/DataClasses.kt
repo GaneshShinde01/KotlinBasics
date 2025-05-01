7d0d82ddbc4bd10208a6ff33a215aa61fcdb820c
ganesh shinde <ganeshss978081@gmail.com>
2025-05-01 18:55:12 +0530

    Data Classes

--- src/main/kotlin/DataClasses.kt
+++ src/main/kotlin/DataClasses.kt
@@ -3,12 +3,16 @@ package org.example
 fun main(){
 
     val p1 = Person1(1,"Ganesh")
-    val p2 = Person1(2,"Ganesh")
+    val p2 = Person1(2,"Krushna")
 
     val p3 = p1.copy()
     val p4 = p1.copy(id=4)
     println(p3)
     println(p4)
+
+    val (id,name) = p2
+    println(id)
+    println(name)
 }
 
 data class Person1(val id : Int, val name : String){
