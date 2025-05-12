5262ee2d6e212bcc1470a8eaba4d152a0bb1c4ae
ganesh shinde <ganeshss978081@gmail.com>
2025-05-12 19:02:13 +0530

    enum

---
+++ src/main/kotlin/Enum2.kt
@@ -0,0 +1,33 @@
+package org.example
+
+enum class TrafficLight(val action : String){
+
+    RED("Thamba"),
+    YELLOW("Veg kami kara"),
+    GREEN("Ja");
+
+    fun getInstructions() : String{
+
+        return "Traffic Light Says: $action"
+    }
+}
+
+fun processTrafficLight(light : TrafficLight){
+
+    when(light) {
+        TrafficLight.RED -> println(light.getInstructions())
+        TrafficLight.YELLOW -> println(light.getInstructions())
+        TrafficLight.GREEN -> println(light.getInstructions())
+    }
+}
+
+fun main() {
+
+    val red = TrafficLight.RED
+    val yellow = TrafficLight.YELLOW
+    val green = TrafficLight.GREEN
+
+    processTrafficLight(red)
+    processTrafficLight(yellow)
+    processTrafficLight(green)
+}
